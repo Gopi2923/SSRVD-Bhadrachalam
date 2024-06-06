14a9a45415cfc14f20d462c87bd96d8b052a1748
Gopi2923 <dgopidhikshan@gmail.com>
2024-06-06 15:24:45 +0530

    Format changes.

--- src/Components/SubSevaList/SubSevaList.jsx
+++ src/Components/SubSevaList/SubSevaList.jsx
@@ -1,4 +1,3 @@
-// src/Components/SubSevasList/SubSevasList.jsx
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
