99dfffe13eb261456b09800a82cb7109112ae5e0
Gopi2923 <dgopidhikshan@gmail.com>
2024-06-10 13:23:33 +0530

    Added bg-img and titles.

--- src/Components/WelcomePage/WelcomePage.jsx
+++ src/Components/WelcomePage/WelcomePage.jsx
@@ -15,6 +15,8 @@ const WelcomePage = ({ handleClick }) => {
     <div className="Welcome-page">
       <img src={logo} alt="Temple Logo" className="logo" />
       <h1>SREE SEETHAARAMACHANDRA SWAMY<br /> VAARI DEVASTHANAM - Bhadrachalam</h1>
+      <h2 className='telugu-title'>శ్రీ సీతారామచంద్ర స్వామి వారి దేవస్థానం</h2>
+      <h3 className='telugu-title2'>భద్రాచలం, భద్రాద్రి కొత్తగూడెం జిల్లా</h3>
       <button onClick={handleButtonClick} className="welcome-btn">
         Click here to get tickets <img src="/src/assets/arrow_icon.png" alt="" />
       </button>
